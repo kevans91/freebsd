50a21af6d4c7aa45704df4394aee685d8cb7bdac
Kyle Evans <kevans@FreeBSD.org>
2019-04-28 21:10:56 -0500

    Restore BASED ON from if_tap.c

--- sys/net/if_tuntap.c
+++ sys/net/if_tuntap.c
@@ -27,9 +27,10 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- */
-
-/*-
+ *
+ * BASED ON:
+ * -------------------------------------------------------------------------
+ *
  * Copyright (c) 1988, Julian Onions <jpo@cs.nott.ac.uk>
  * Nottingham University 1987.
  *
