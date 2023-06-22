9d7d18b8253f2923361ea17a54834f2550e0651f
Mashijams <raghavself28@gmail.com>
2023-06-22 14:24:13 +0530

    squashfs: add zstd decompressor support

--- sys/fs/squashfs/squashfs_decompressor.h
+++ sys/fs/squashfs/squashfs_decompressor.h
@@ -56,6 +56,10 @@ extern const struct sqsh_decompressor sqsh_lzo_decompressor;
 extern const struct sqsh_decompressor sqsh_lz4_decompressor;
 #endif
 
+#ifdef SQUASHFS_ZSTD
+extern const struct sqsh_decompressor sqsh_zstd_decompressor;
+#endif
+
 const struct sqsh_decompressor *sqsh_lookup_decompressor(int id);
 
 #endif // SQUASHFS_DECOMPRESSOR_H
