8a05eb93a6ff4e55d744db2c60c455b41476f6f6
Mashijams <raghavself28@gmail.com>
2023-08-15 15:20:42 +0530

    squashfs: Implement sqsh_blockidx_indexable() function to check if we can index file block
    (cherry picked from commit 19405a2a1fe1243525b89dbd249dd82e1ace2a03)

--- sys/fs/squashfs/squashfs_file.h
+++ sys/fs/squashfs/squashfs_file.h
@@ -51,6 +51,13 @@ struct sqsh_blockidx_entry {
 size_t		sqsh_blocklist_count(struct sqsh_mount *ump, struct sqsh_inode *inode);
 void		sqsh_blocklist_init(struct sqsh_mount *ump, struct sqsh_inode *inode,
 				struct sqsh_blocklist *bl);
-sqfs_err	sqsh_blocklist_next(struct sqsh_blocklist *bl);
+sqsh_err	sqsh_blocklist_next(struct sqsh_blocklist *bl);
+
+/* Block index helper functions for skipping to the middle of large files */
+static bool	sqsh_blockidx_indexable(struct sqsh_mount *ump, struct sqsh_inode *inode);
+sqsh_err	sqsh_blockidx_add(struct sqsh_mount *ump, struct sqsh_inode *inode,
+				struct sqsh_blockidx_entry *out);
+sqsh_err	sqsh_blockidx_blocklist(struct sqsh_mount *ump, struct sqsh_inode *inode,
+				struct sqsh_blocklist *bl, off_t start);
 
 #endif
