8f5b4f810a429082b9f4c722ff27b12cec461c40
Kyle Evans <kevans@FreeBSD.org>
2023-11-23 00:55:49 -0600

    loader: lua: assume late ACPI detection if the feature isn't enabled
    While we're here, enable the feature in the places we detect ACPI.  This
    lets us side-step the existing issues and provide a path forward for
    folks upgrading from previous releases that haven't updated their ESP
    yet.
    
    Differential Revision:	https://reviews.freebsd.org/D42727

--- stand/lua/core.lua
+++ stand/lua/core.lua
@@ -133,17 +133,20 @@ function core.setSingleUser(single_user)
 end
 
 function core.hasACPI()
-	return loader.getenv("acpi.rsdp") ~= nil
-end
+	-- We can't trust acpi.rsdp to be set if the loader binary doesn't do
+	-- ACPI detection early enough.  UEFI loader historically didn't, so
+	-- we'll fallback to assuming ACPI is enabled if this binary does not
+	-- declare that it probes for ACPI early enough
+	if loader.getenv("acpi.rsdp") ~= nil then
+		return true
+	end
 
-function core.isX86()
-	return loader.machine_arch == "i386" or loader.machine_arch == "amd64"
+	return not core.hasFeature("EARLY_ACPI")
 end
 
 function core.getACPI()
 	if not core.hasACPI() then
-		-- x86 requires ACPI pretty much
-		return false or core.isX86()
+		return false
 	end
 
 	-- Otherwise, respect disabled if it's set
