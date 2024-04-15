freebsd/freebsd-src

commit de13c2427d6c36e498f075cc9f895d4484d2a702

libmd: introduce functions that operate on an fd instead of filename
Reviewed by:	allanjude, cem
MFC after:	2 months
Sponsored by:	The FreeBSD Foundation
Differential Revision:	https://reviews.freebsd.org/D8264

lib/libmd/mdXhl.c
@@ -42,18 +42,18 @@ MDXEnd(MDX_CTX *ctx, char *buf)
 }
 
 char *
-MDXFile(const char *filename, char *buf)
+MDXFd(int fd, char *buf)
 {
-	return (MDXFileChunk(filename, buf, 0, 0));
+	return MDXFdChunk(fd, buf, 0, 0);
 }
 
 char *
-MDXFileChunk(const char *filename, char *buf, off_t ofs, off_t len)
+MDXFdChunk(int fd, char *buf, off_t ofs, off_t len)
 {
 	unsigned char buffer[16*1024];
 	MDX_CTX ctx;
 	struct stat stbuf;
-	int fd, readrv, e;
+	int readrv, e;
 	off_t remain;
 
 	if (len < 0) {
@@ -62,9 +62,6 @@ MDXFileChunk(const char *filename, char *buf, off_t ofs, off_t len)
 	}
 
 	MDXInit(&ctx);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return NULL;
 	if (ofs != 0) {
 		errno = 0;
 		if (lseek(fd, ofs, SEEK_SET) != ofs ||
@@ -86,12 +83,31 @@ MDXFileChunk(const char *filename, char *buf, off_t ofs, off_t len)
 		remain -= readrv;
 	} 
 error:
-	e = errno;
-	close(fd);
-	errno = e;
 	if (readrv < 0)
 		return NULL;
 	return (MDXEnd(&ctx, buf));
+}
+
+char *
+MDXFile(const char *filename, char *buf)
+{
+	return (MDXFileChunk(filename, buf, 0, 0));
+}
+
+char *
+MDXFileChunk(const char *filename, char *buf, off_t ofs, off_t len)
+{
+	char *ret;
+	int e, fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return NULL;
+	ret = MDXFdChunk(fd, buf, ofs, len);
+	e = errno;
+	close (fd);
+	errno = e;
+	return ret;
 }
 
 char *

lib/libmd/md4.h
@@ -53,6 +53,12 @@ __BEGIN_DECLS
 #ifndef MD4End
 #define MD4End		_libmd_MD4End
 #endif
+#ifndef MD4Fd
+#define MD4Fd		_libmd_MD4Fd
+#endif
+#ifndef MD4FdChunk
+#define MD4FdChunk	_libmd_MD4FdChunk
+#endif
 #ifndef MD4File
 #define MD4File		_libmd_MD4File
 #endif
@@ -68,6 +74,8 @@ void   MD4Update(MD4_CTX *, const void *, unsigned int);
 void   MD4Pad(MD4_CTX *);
 void   MD4Final(unsigned char [16], MD4_CTX *);
 char * MD4End(MD4_CTX *, char *);
+char * MD4Fd(int, char *);
+char * MD4FdChunk(int, char *, off_t, off_t);
 char * MD4File(const char *, char *);
 char * MD4FileChunk(const char *, char *, off_t, off_t);
 char * MD4Data(const void *, unsigned int, char *);

lib/libmd/md5.h
@@ -25,6 +25,12 @@
 #ifndef MD5End
 #define MD5End		_libmd_MD5End
 #endif
+#ifndef MD5Fd
+#define MD5Fd		_libmd_MD5Fd
+#endif
+#ifndef MD5FdChunk
+#define MD5FdChunk	_libmd_MD5FdChunk
+#endif
 #ifndef MD5File
 #define MD5File		_libmd_MD5File
 #endif

lib/libmd/ripemd.h
@@ -96,6 +96,12 @@ __BEGIN_DECLS
 #ifndef RIPEMD160_End
 #define RIPEMD160_End		_libmd_RIPEMD160_End
 #endif
+#ifndef RIPEMD160_Fd
+#define RIPEMD160_Fd		_libmd_RIPEMD160_Fd
+#endif
+#ifndef RIPEMD160_FdChunk
+#define RIPEMD160_FdChunk	_libmd_RIPEMD160_FdChunk
+#endif
 #ifndef RIPEMD160_File
 #define RIPEMD160_File		_libmd_RIPEMD160_File
 #endif
@@ -121,6 +127,8 @@ void	RIPEMD160_Update(RIPEMD160_CTX *c, const void *data,
 			 size_t len);
 void	RIPEMD160_Final(unsigned char *md, RIPEMD160_CTX *c);
 char   *RIPEMD160_End(RIPEMD160_CTX *, char *);
+char   *RIPEMD160_Fd(int, char *);
+char   *RIPEMD160_FdChunk(int, char *, off_t, off_t);
 char   *RIPEMD160_File(const char *, char *);
 char   *RIPEMD160_FileChunk(const char *, char *, off_t, off_t);
 char   *RIPEMD160_Data(const void *, unsigned int, char *);

lib/libmd/sha.h
@@ -94,6 +94,12 @@ __BEGIN_DECLS
 #ifndef SHA_End
 #define SHA_End		_libmd_SHA_End
 #endif
+#ifndef SHA_Fd
+#define SHA_Fd		_libmd_SHA_Fd
+#endif
+#ifndef SHA_FdChunk
+#define SHA_FdChunk	_libmd_SHA_FdChunk
+#endif
 #ifndef SHA_File
 #define SHA_File	_libmd_SHA_File
 #endif
@@ -126,6 +132,12 @@ __BEGIN_DECLS
 #ifndef SHA1_End
 #define SHA1_End	_libmd_SHA1_End
 #endif
+#ifndef SHA1_Fd
+#define SHA1_Fd		_libmd_SHA1_Fd
+#endif
+#ifndef SHA1_FdChunk
+#define SHA1_FdChunk	_libmd_SHA1_FdChunk
+#endif
 #ifndef SHA1_File
 #define SHA1_File	_libmd_SHA1_File
 #endif
@@ -150,6 +162,8 @@ void	SHA_Init(SHA_CTX *c);
 void	SHA_Update(SHA_CTX *c, const void *data, size_t len);
 void	SHA_Final(unsigned char *md, SHA_CTX *c);
 char   *SHA_End(SHA_CTX *, char *);
+char   *SHA_Fd(int, char *);
+char   *SHA_FdChunk(int, char *, off_t, off_t);
 char   *SHA_File(const char *, char *);
 char   *SHA_FileChunk(const char *, char *, off_t, off_t);
 char   *SHA_Data(const void *, unsigned int, char *);
@@ -158,6 +172,8 @@ void	SHA1_Init(SHA_CTX *c);
 void	SHA1_Update(SHA_CTX *c, const void *data, size_t len);
 void	SHA1_Final(unsigned char *md, SHA_CTX *c);
 char   *SHA1_End(SHA_CTX *, char *);
+char   *SHA1_Fd(int, char *);
+char   *SHA1_FdChunk(int, char *, off_t, off_t);
 char   *SHA1_File(const char *, char *);
 char   *SHA1_FileChunk(const char *, char *, off_t, off_t);
 char   *SHA1_Data(const void *, unsigned int, char *);

sys/crypto/sha2/sha256.h
@@ -59,6 +59,12 @@ __BEGIN_DECLS
 #ifndef SHA256_End
 #define SHA256_End		_libmd_SHA256_End
 #endif
+#ifndef SHA256_Fd
+#define SHA256_Fd		_libmd_SHA256_Fd
+#endif
+#ifndef SHA256_FdChunk
+#define SHA256_FdChunk		_libmd_SHA256_FdChunk
+#endif
 #ifndef SHA256_File
 #define SHA256_File		_libmd_SHA256_File
 #endif
@@ -82,6 +88,8 @@ void	SHA256_Final(unsigned char [static SHA256_DIGEST_LENGTH], SHA256_CTX *);
 #ifndef _KERNEL
 char   *SHA256_End(SHA256_CTX *, char *);
 char   *SHA256_Data(const void *, unsigned int, char *);
+char   *SHA256_Fd(int, char *);
+char   *SHA256_FdChunk(int, char *, off_t, off_t);
 char   *SHA256_File(const char *, char *);
 char   *SHA256_FileChunk(const char *, char *, off_t, off_t);
 #endif

sys/crypto/sha2/sha384.h
@@ -58,6 +58,12 @@ __BEGIN_DECLS
 #ifndef SHA384_End
 #define SHA384_End		_libmd_SHA384_End
 #endif
+#ifndef SHA384_Fd
+#define SHA384_Fd		_libmd_SHA384_Fd
+#endif
+#ifndef SHA384_FdChunk
+#define SHA384_FdChunk		_libmd_SHA384_FdChunk
+#endif
 #ifndef SHA384_File
 #define SHA384_File		_libmd_SHA384_File
 #endif
@@ -78,6 +84,8 @@ void	SHA384_Final(unsigned char [static SHA384_DIGEST_LENGTH], SHA384_CTX *);
 #ifndef _KERNEL
 char   *SHA384_End(SHA384_CTX *, char *);
 char   *SHA384_Data(const void *, unsigned int, char *);
+char   *SHA384_Fd(int, char *);
+char   *SHA384_FdChunk(int, char *, off_t, off_t);
 char   *SHA384_File(const char *, char *);
 char   *SHA384_FileChunk(const char *, char *, off_t, off_t);
 #endif

sys/crypto/sha2/sha512.h
@@ -58,6 +58,12 @@ __BEGIN_DECLS
 #ifndef SHA512_End
 #define SHA512_End		_libmd_SHA512_End
 #endif
+#ifndef SHA512_Fd
+#define SHA512_Fd		_libmd_SHA512_Fd
+#endif
+#ifndef SHA512_FdChunk
+#define SHA512_FdChunk		_libmd_SHA512_FdChunk
+#endif
 #ifndef SHA512_File
 #define SHA512_File		_libmd_SHA512_File
 #endif
@@ -81,6 +87,8 @@ void	SHA512_Final(unsigned char [static SHA512_DIGEST_LENGTH], SHA512_CTX *);
 #ifndef _KERNEL
 char   *SHA512_End(SHA512_CTX *, char *);
 char   *SHA512_Data(const void *, unsigned int, char *);
+char   *SHA512_Fd(int, char *);
+char   *SHA512_FdChunk(int, char *, off_t, off_t);
 char   *SHA512_File(const char *, char *);
 char   *SHA512_FileChunk(const char *, char *, off_t, off_t);
 #endif

kevans91/freebsd

commit ce3e038298fec61bdb79d8c9b5b3dc65cfe6580c

libmd: export and document *Fd/*FdChunk interfaces
Fixes: de13c2427d6c ("libmd: introduce functions that operate on an fd")

Differential Revision:	https://reviews.freebsd.org/D34502

lib/libmd/mdX.3
@@ -6,7 +6,7 @@
 .\" this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
 .\" ----------------------------------------------------------------------------
 .\"
-.Dd May 21, 2019
+.Dd March 8, 2022
 .Dt MDX 3
 .Os
 .Sh NAME
@@ -15,6 +15,8 @@
 .Nm MDXPad ,
 .Nm MDXFinal ,
 .Nm MDXEnd ,
+.Nm MDXFd ,
+.Nm MDXFdChunk ,
 .Nm MDXFile ,
 .Nm MDXFileChunk ,
 .Nm MDXData
@@ -34,6 +36,10 @@
 .Fn MDXFinal "unsigned char digest[16]" "MDX_CTX *context"
 .Ft "char *"
 .Fn MDXEnd "MDX_CTX *context" "char *buf"
+.Ft "char *"
+.Fn MDXFd "int fd" "char *buf"
+.Ft "char *"
+.Fn MDXFdChunk "int fd" "char *buf" "off_t offset" "off_t length"
 .Ft "char *"
 .Fn MDXFile "const char *filename" "char *buf"
 .Ft "char *"
@@ -124,6 +130,19 @@ The
 function calculates the digest of a chunk of data in memory, and uses
 .Fn MDXEnd
 to return the result.
+.Pp
+The
+.Fn MDXFd
+and
+.Fn MDXFdChunk
+functions are identical to their
+.Fn MDXFile
+and
+.Fn MDXFileChunk
+counterparts, with the exception that the first argument is an
+.Fa fd
+instead of a
+.Fa filename .
 .Pp
 When using
 .Fn MDXEnd ,

lib/libmd/mdXhl.c
@@ -130,6 +130,10 @@ __weak_reference(_libmd_MDXEnd, MDXEnd);
 __weak_reference(_libmd_MDXFile, MDXFile);
 #undef MDXFileChunk
 __weak_reference(_libmd_MDXFileChunk, MDXFileChunk);
+#undef MDXFd
+__weak_reference(_libmd_MDXFd, MDXFd);
+#undef MDXFdChunk
+__weak_reference(_libmd_MDXFdChunk, MDXFdChunk);
 #undef MDXData
 __weak_reference(_libmd_MDXData, MDXData);
 #endif

lib/libmd/ripemd.3
@@ -8,7 +8,7 @@
 .\"
 .\" 	From: Id: mdX.3,v 1.14 1999/02/11 20:31:49 wollman Exp
 .\"
-.Dd July 20, 2018
+.Dd March 8, 2022
 .Dt RIPEMD 3
 .Os
 .Sh NAME
@@ -16,6 +16,8 @@
 .Nm RIPEMD160_Update ,
 .Nm RIPEMD160_Final ,
 .Nm RIPEMD160_End ,
+.Nm RIPEMD160_Fd ,
+.Nm RIPEMD160_FdChunk ,
 .Nm RIPEMD160_File ,
 .Nm RIPEMD160_FileChunk ,
 .Nm RIPEMD160_Data
@@ -33,6 +35,10 @@
 .Fn RIPEMD160_Final "unsigned char digest[20]" "RIPEMD160_CTX *context"
 .Ft "char *"
 .Fn RIPEMD160_End "RIPEMD160_CTX *context" "char *buf"
+.Ft "char *"
+.Fn RIPEMD160_Fd "int fd" "char *buf"
+.Ft "char *"
+.Fn RIPEMD160_FdChunk "int fd" "char *buf" "off_t offset" "off_t length"
 .Ft "char *"
 .Fn RIPEMD160_File "const char *filename" "char *buf"
 .Ft "char *"
@@ -105,6 +111,19 @@ The
 function calculates the digest of a chunk of data in memory, and uses
 .Fn RIPEMD160_End
 to return the result.
+.Pp
+The
+.Fn RIPEMD160_Fd
+and
+.Fn RIPEMD160_FdChunk
+functions are identical to their
+.Fn RIPEMD160_File
+and
+.Fn RIPEMD160_FileChunk
+counterparts, with the exception that the first argument is an
+.Fa fd
+instead of a
+.Fa filename .
 .Pp
 When using
 .Fn RIPEMD160_End ,

lib/libmd/sha.3
@@ -8,7 +8,7 @@
 .\"
 .\" 	From: Id: mdX.3,v 1.14 1999/02/11 20:31:49 wollman Exp
 .\"
-.Dd February 6, 2023
+.Dd February 12, 2023
 .Dt SHA 3
 .Os
 .Sh NAME
@@ -16,6 +16,8 @@
 .Nm SHA_Update ,
 .Nm SHA_Final ,
 .Nm SHA_End ,
+.Nm SHA_Fd ,
+.Nm SHA_FdChunk ,
 .Nm SHA_File ,
 .Nm SHA_FileChunk ,
 .Nm SHA_Data ,
@@ -23,6 +25,8 @@
 .Nm SHA1_Update ,
 .Nm SHA1_Final ,
 .Nm SHA1_End ,
+.Nm SHA1_Fd ,
+.Nm SHA1_FdChunk ,
 .Nm SHA1_File ,
 .Nm SHA1_FileChunk ,
 .Nm SHA1_Data
@@ -41,6 +45,10 @@
 .Ft "char *"
 .Fn SHA_End "SHA_CTX *context" "char *buf"
 .Ft "char *"
+.Fn SHA_Fd "int fd" "char *buf"
+.Ft "char *"
+.Fn SHA_FdChunk "int fd" "char *buf" "off_t offset" "off_t length"
+.Ft "char *"
 .Fn SHA_File "const char *filename" "char *buf"
 .Ft "char *"
 .Fn SHA_FileChunk "const char *filename" "char *buf" "off_t offset" "off_t length"
@@ -54,6 +62,10 @@
 .Fn SHA1_Final "unsigned char digest[20]" "SHA_CTX *context"
 .Ft "char *"
 .Fn SHA1_End "SHA_CTX *context" "char *buf"
+.Ft "char *"
+.Fn SHA1_Fd "int fd" "char *buf"
+.Ft "char *"
+.Fn SHA1_FdChunk "int fd" "char *buf" "off_t offset" "off_t length"
 .Ft "char *"
 .Fn SHA1_File "const char *filename" "char *buf"
 .Ft "char *"
@@ -129,6 +141,19 @@ to the end of file.
 calculates the digest of a chunk of data in memory, and uses
 .Fn SHA1_End
 to return the result.
+.Pp
+The
+.Fn SHA1_Fd
+and
+.Fn SHA1_FdChunk
+functions are identical to their
+.Fn SHA1_File
+and
+.Fn SHA1_FileChunk
+counterparts, with the exception that the first argument is an
+.Fa fd
+instead of a
+.Fa filename .
 .Pp
 When using
 .Fn SHA1_End ,

lib/libmd/sha256.3
@@ -8,7 +8,7 @@
 .\"
 .\" 	From: Id: mdX.3,v 1.14 1999/02/11 20:31:49 wollman Exp
 .\"
-.Dd July 20, 2018
+.Dd March 8, 2022
 .Dt SHA256 3
 .Os
 .Sh NAME
@@ -16,6 +16,8 @@
 .Nm SHA224_Update ,
 .Nm SHA224_Final ,
 .Nm SHA224_End ,
+.Nm SHA224_Fd ,
+.Nm SHA224_FdChunk ,
 .Nm SHA224_File ,
 .Nm SHA224_FileChunk ,
 .Nm SHA224_Data ,
@@ -23,6 +25,8 @@
 .Nm SHA256_Update ,
 .Nm SHA256_Final ,
 .Nm SHA256_End ,
+.Nm SHA256_Fd ,
+.Nm SHA256_FdChunk ,
 .Nm SHA256_File ,
 .Nm SHA256_FileChunk ,
 .Nm SHA256_Data
@@ -41,6 +45,10 @@
 .Ft "char *"
 .Fn SHA224_End "SHA224_CTX *context" "char *buf"
 .Ft "char *"
+.Fn SHA224_Fd "int fd" "char *buf"
+.Ft "char *"
+.Fn SHA224_FdChunk "int fd" "char *buf" "off_t offset" "off_t length"
+.Ft "char *"
 .Fn SHA224_File "const char *filename" "char *buf"
 .Ft "char *"
 .Fn SHA224_FileChunk "const char *filename" "char *buf" "off_t offset" "off_t length"
@@ -55,6 +63,10 @@
 .Fn SHA256_Final "unsigned char digest[32]" "SHA256_CTX *context"
 .Ft "char *"
 .Fn SHA256_End "SHA256_CTX *context" "char *buf"
+.Ft "char *"
+.Fn SHA256_Fd "int fd" "char *buf"
+.Ft "char *"
+.Fn SHA256_FdChunk "int fd" "char *buf" "off_t offset" "off_t length"
 .Ft "char *"
 .Fn SHA256_File "const char *filename" "char *buf"
 .Ft "char *"
@@ -124,6 +136,19 @@ to the end of file.
 calculates the digest of a chunk of data in memory, and uses
 .Fn SHA256_End
 to return the result.
+.Pp
+The
+.Fn SHA256_Fd
+and
+.Fn SHA256_FdChunk
+functions are identical to their
+.Fn SHA256_File
+and
+.Fn SHA256_FileChunk
+counterparts, with the exception that the first argument is an
+.Fa fd
+instead of a
+.Fa filename .
 .Pp
 When using
 .Fn SHA256_End ,

lib/libmd/sha512.3
@@ -8,7 +8,7 @@
 .\"
 .\" 	From: Id: mdX.3,v 1.14 1999/02/11 20:31:49 wollman Exp
 .\"
-.Dd February 3, 2023
+.Dd February 12, 2023
 .Dt SHA512 3
 .Os
 .Sh NAME
@@ -16,6 +16,8 @@
 .Nm SHA512_Update ,
 .Nm SHA512_Final ,
 .Nm SHA512_End ,
+.Nm SHA512_Fd ,
+.Nm SHA512_FdChunk ,
 .Nm SHA512_File ,
 .Nm SHA512_FileChunk ,
 .Nm SHA512_Data ,
@@ -23,6 +25,8 @@
 .Nm SHA384_Update ,
 .Nm SHA384_Final ,
 .Nm SHA384_End ,
+.Nm SHA384_Fd ,
+.Nm SHA384_FdChunk ,
 .Nm SHA384_File ,
 .Nm SHA384_FileChunk ,
 .Nm SHA384_Data ,
@@ -37,6 +41,8 @@
 .Nm SHA512_256_Update ,
 .Nm SHA512_256_Final ,
 .Nm SHA512_256_End ,
+.Nm SHA512_256_Fd ,
+.Nm SHA512_256_FdChunk ,
 .Nm SHA512_256_File ,
 .Nm SHA512_256_FileChunk ,
 .Nm SHA512_256_Data
@@ -55,6 +61,10 @@
 .Ft "char *"
 .Fn SHA512_End "SHA512_CTX *context" "char *buf"
 .Ft "char *"
+.Fn SHA512_Fd "int fd" "char *buf"
+.Ft "char *"
+.Fn SHA512_FdChunk "int fd" "char *buf" "off_t offset" "off_t length"
+.Ft "char *"
 .Fn SHA512_File "const char *filename" "char *buf"
 .Ft "char *"
 .Fn SHA512_FileChunk "const char *filename" "char *buf" "off_t offset" "off_t length"
@@ -69,6 +79,10 @@
 .Fn SHA384_Final "unsigned char digest[48]" "SHA384_CTX *context"
 .Ft "char *"
 .Fn SHA384_End "SHA384_CTX *context" "char *buf"
+.Ft "char *"
+.Fn SHA384_Fd "int fd" "char *buf"
+.Ft "char *"
+.Fn SHA384_FdChunk "int fd" "char *buf" "off_t offset" "off_t length"
 .Ft "char *"
 .Fn SHA384_File "const char *filename" "char *buf"
 .Ft "char *"
@@ -98,6 +112,10 @@
 .Fn SHA512_256_Final "unsigned char digest[32]" "SHA512_CTX *context"
 .Ft "char *"
 .Fn SHA512_256_End "SHA512_CTX *context" "char *buf"
+.Ft "char *"
+.Fn SHA512_256_Fd "int fd" "char *buf"
+.Ft "char *"
+.Fn SHA512_256_FdChunk "int fd" "char *buf" "off_t offset" "off_t length"
 .Ft "char *"
 .Fn SHA512_256_File "const char *filename" "char *buf"
 .Ft "char *"
@@ -167,6 +185,19 @@ to the end of file.
 calculates the digest of a chunk of data in memory, and uses
 .Fn SHA512_End
 to return the result.
+.Pp
+The
+.Fn SHA512_Fd
+and
+.Fn SHA512_FdChunk
+functions are identical to their
+.Fn SHA512_File
+and
+.Fn SHA512_FileChunk
+counterparts, with the exception that the first argument is an
+.Fa fd
+instead of a
+.Fa filename .
 .Pp
 When using
 .Fn SHA512_End ,

lib/libmd/skein.3
@@ -23,7 +23,7 @@
 .\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 .\" SUCH DAMAGE.
 .\"
-.Dd May 21, 2019
+.Dd March 8, 2022
 .Dt SKEIN 3
 .Os
 .Sh NAME
@@ -31,6 +31,8 @@
 .Nm SKEIN256_Update ,
 .Nm SKEIN256_Final ,
 .Nm SKEIN256_End ,
+.Nm SKEIN256_Fd ,
+.Nm SKEIN256_FdChunk ,
 .Nm SKEIN256_File ,
 .Nm SKEIN256_FileChunk ,
 .Nm SKEIN256_Data ,
@@ -38,6 +40,8 @@
 .Nm SKEIN512_Update ,
 .Nm SKEIN512_Final ,
 .Nm SKEIN512_End ,
+.Nm SKEIN512_Fd ,
+.Nm SKEIN512_FdChunk ,
 .Nm SKEIN512_File ,
 .Nm SKEIN512_FileChunk ,
 .Nm SKEIN512_Data ,
@@ -45,6 +49,8 @@
 .Nm SKEIN1024_Update ,
 .Nm SKEIN1024_Final ,
 .Nm SKEIN1024_End ,
+.Nm SKEIN1024_Fd ,
+.Nm SKEIN1024_FdChunk ,
 .Nm SKEIN1024_File ,
 .Nm SKEIN1024_FileChunk ,
 .Nm SKEIN1024_Data
@@ -63,6 +69,10 @@
 .Ft "char *"
 .Fn SKEIN256_End "SKEIN256_CTX *context" "char *buf"
 .Ft "char *"
+.Fn SKEIN256_Fd "int fd" "char *buf"
+.Ft "char *"
+.Fn SKEIN256_FdChunk "int fd" "char *buf" "off_t offset" "off_t length"
+.Ft "char *"
 .Fn SKEIN256_File "const char *filename" "char *buf"
 .Ft "char *"
 .Fn SKEIN256_FileChunk "const char *filename" "char *buf" "off_t offset" "off_t length"
@@ -77,6 +87,10 @@
 .Ft "char *"
 .Fn SKEIN512_End "SKEIN512_CTX *context" "char *buf"
 .Ft "char *"
+.Fn SKEIN512_Fd "int fd" "char *buf"
+.Ft "char *"
+.Fn SKEIN512_FdChunk "int fd" "char *buf" "off_t offset" "off_t length"
+.Ft "char *"
 .Fn SKEIN512_File "const char *filename" "char *buf"
 .Ft "char *"
 .Fn SKEIN512_FileChunk "const char *filename" "char *buf" "off_t offset" "off_t length"
@@ -90,6 +104,10 @@
 .Fn SKEIN1024_Final "unsigned char digest[128]" "SKEIN1024_CTX *context"
 .Ft "char *"
 .Fn SKEIN1024_End "SKEIN1024_CTX *context" "char *buf"
+.Ft "char *"
+.Fn SKEIN1024_Fd "int fd" "char *buf"
+.Ft "char *"
+.Fn SKEIN1024_FdChunk "int fd" "char *buf" "off_t offset" "off_t length"
 .Ft "char *"
 .Fn SKEIN1024_File "const char *filename" "char *buf"
 .Ft "char *"
@@ -159,6 +177,19 @@ to the end of file.
 calculates the digest of a chunk of data in memory, and uses
 .Fn SKEIN256_End
 to return the result.
+.Pp
+The
+.Fn SKEIN256_Fd
+and
+.Fn SKEIN256_FdChunk
+functions are identical to their
+.Fn SKEIN256_File
+and
+.Fn SKEIN256_FileChunk
+counterparts, with the exception that the first argument is an
+.Fa fd
+instead of a
+.Fa filename .
 .Pp
 When using
 .Fn SKEIN256_End ,