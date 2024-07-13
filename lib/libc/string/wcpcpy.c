92473032eb697e2d8bd5201804fe34d0d28f62f9
Kyle Evans <kevans@FreeBSD.org>
2024-07-13 00:16:11 -0500

    include: ssp: fortify <wchar.h>
    This includes all of the w*() equivalents to str*()/mem*() implemented
    in more or less the same way.  For these ones, we'll just use
    header-only implementations from the start to stop further cluttering
    the libc symbol table.
    
    Reviewed by:	markj
    Sponsored by:	Klara, Inc.
    Sponsored by:	Stormshield
    Differential Revision:	https://reviews.freebsd.org/D45682
    
    (cherry picked from commit b53d7aa88fd5a5e8a67dd71ea7332fa473595d28)

--- lib/libc/string/wcpcpy.c
+++ lib/libc/string/wcpcpy.c
@@ -35,9 +35,10 @@
 static char sccsid[] = "@(#)strcpy.c	8.1 (Berkeley) 6/4/93";
 #endif /* LIBC_SCCS and not lint */
 #include <wchar.h>
+#include <ssp/ssp.h>
 
 wchar_t *
-wcpcpy(wchar_t * __restrict to, const wchar_t * __restrict from)
+__ssp_real(wcpcpy)(wchar_t * __restrict to, const wchar_t * __restrict from)
 {
 
 	for (; (*to = *from); ++from, ++to);
