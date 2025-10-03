f421e80ce564c9b037ea26e37d79cfb06961bdce
Kyle Evans <kevans@FreeBSD.org>
2025-10-03 14:43:41 -0500

    pkgbase: switch to a PATH search for flua
    Similar to how the top-level `make sysent` does it, inject libexec into
    $PATH and search it for flua instead of relying on a hardcoded flua.
    
    This gives us the flexibility to use a bootstrap flua if it has been
    built instead of the system flua while still using the system flua where
    it's been deemed adequate.

--- release/packages/generate-ucl.sh
+++ release/packages/generate-ucl.sh
@@ -82,7 +82,8 @@ main() {
 	fi
 
 	cap_arg="$( make -f ${srctree}/share/mk/bsd.endian.mk -VCAP_MKDB_ENDIAN )"
-	${srctree}/release/packages/generate-ucl.lua \
+	# Makefile.inc1 should position flua in PATH for us
+	flua ${srctree}/release/packages/generate-ucl.lua \
 		VERSION "${PKG_VERSION}" \
 		PKGNAME "${origname}" \
 		PKGGENNAME "${outname}" \
